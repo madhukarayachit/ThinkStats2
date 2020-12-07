bbf2077a9be9c73e4ce70941ef76b09f11d3cf8c
madhukarayachit <66280862+madhukarayachit@users.noreply.github.com>
2020-12-07 15:53:17 -0500

    Add files via upload

---
+++ Exercise/Exercise2.1.py
@@ -0,0 +1,20 @@
+
+# Exercise 2.1
+# Madhukar Ayachit
+# DSC 530
+
+
+#Display the text “Hello World! I wonder why that is always the default coding text to start with”
+print ("Hello World!")
+
+print (2 + 3 )#Add two numbers together
+print (5 - 2 )#Subtract a number from another number
+print(2 * 6) #Multiply two numbers
+print(24/3 ) #Divide between two numbers
+print("Hello " + "World!") # Concatenate two strings together (any words)
+MyList =["Hello","Python",12,23.6]  #Create a list of 4 items (can be strings, numbers, both)
+MyList.append("DSC530")  #Append an item to your list (again, can be a string, number)
+my_tuple = ("Madhukar", "Ayachit", 48, "Solution Architect") #Create a tuple with 4 items (can be strings, numbers, both)
+
+print(MyList)
+print(my_tuple)
